b5895312d378aeca180322b3548aeda522e9aee6
yaemtaUni <yaemta@gmail.com>
2021-08-20 12:14:57 +0300

    first commit

---
+++ src/main/java/com/example/hw2/controller/InstructorController.java
@@ -0,0 +1,9 @@
+package com.example.hw2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("instructors")
+public class InstructorController {
+}
